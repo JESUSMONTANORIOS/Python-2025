2529b4bbb2770d7643c3f83cc91ad04e33236824
JESUSMONTANORIOS <jesusmontanorios@gmail.com>
2025-03-16 18:25:29 -0700

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,39 @@
+'''
+Print Hello World to the Screen
+By Jesus M
+'''
+
+import os
+os.system('clear') # This is another comment
+
+first_name = 'Jesus' 
+print (first_name) 
+
+first_name = "" 
+print (first_name) 
+
+first_name = "Bob" # String
+age=45 # Number
+names = ["Jhon","Bob","Mary"] # This is a list and index starts at 0
+print (names[0])
+
+#Tuple is a list you can't change
+names = ("Jhon","Bob","Mary") # This is a tuple
+print (names)
+print (names[1])
+
+#Dictionaries "key" : "value"
+fav_pizza={
+	"Jhon" : "Pepperoni",
+	"Bob" : "Mushroon",
+	"Mary":"Cheese"
+	}
+
+print (fav_pizza["Jhon"])
+
+#Boolean True or False
+
+ 
+
+
+
